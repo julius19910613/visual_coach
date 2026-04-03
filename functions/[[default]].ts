4a8887605d4c97151baba84e90609468d8a0e9a2
julius19910613 <julius19910613@users.noreply.github.com>
2026-04-03 21:28:41 +0800

    fix: use EdgeOne Pages node-functions directory (Node Functions mode)
    - Move api/[[default]].ts -> node-functions/[[default]].js
    - Entry imports compiled dist/src/index.js
    - Remove old functions/ and api/ directories

--- functions/[[default]].ts
+++
@@ -1,8 +0,0 @@
-/**
- * EdgeOne Pages entry point.
- * Uses hono-edgeone-pages-adapter to handle requests.
- */
-import { handle } from 'hono-edgeone-pages-adapter';
-import app from '../src/index';
-
-export const onRequest = handle(app);
---
+++ node-functions/[[default]].js
@@ -0,0 +1,2 @@
+import app from "../dist/src/index.js";
+export default app;
